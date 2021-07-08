db822017b367b85d719e7d9da8d76eddd51a4b32
thecomalley <31399219+thecomalley@users.noreply.github.com>
2021-07-08 20:18:09 +1000

    add version constraint syntax

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = "v1.0.1"
+  required_version = ">= v1.0.1"
   backend "azurerm" {}
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "2.23.0"
+      version = ">= 2.23.0"
     }
     uptimerobot = {
       source  = "louy/uptimerobot"
-      version = "0.5.1"
+      version = ">= 0.5.1"
     }
   }
 }
